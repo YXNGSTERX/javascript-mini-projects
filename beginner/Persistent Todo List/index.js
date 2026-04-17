f294bebe13d786b3004613078cab106bced09d39
adam baha <yxngsterx@adams-MacBook-Air.local>
2026-04-17 12:15:18 +0800

    Initial commit: Set up drumkit HTML structure and basic audio playback

--- beginner/Persistent Todo List/index.js
+++ beginner/Persistent Todo List/index.js
@@ -1,6 +1,18 @@
     let items = ['foo', 'bar'];
 
     const itemsDiv = document.getElementById('items');
+    const input = document.getElementById('text');
+    const storageKey = 'todoItems';
+
+    function loadItems() {
+        const stringifiedItems = localStorage.getItem(storageKey);
+        if (stringifiedItems) {
+            items = JSON.parse(stringifiedItems);
+        }
+        renderItems();
+    }
+
+    function renderItems() {
         itemsDiv.innerHTML = null;
 
         for (const [index, item] of items.entries()) {
@@ -9,9 +21,11 @@
             container.style.marginBottom = '10px';
 
             const text = document.createElement('div');
+            text.style.marginRight = '10px';
             text.textContent = item;
 
             const deleteButton = document.createElement('button');
+            deleteButton.style.display = 'inline';
             deleteButton.textContent = 'Delete';
             deleteButton.onclick = () => deleteItem(index);
 
@@ -20,16 +34,30 @@
             itemsDiv.appendChild(container);
 
         }
-
-
-    function loadItems() {
-
     }
 
-    function renderItems() {}
+    function saveItems() {
+        const stringifiedItems = JSON.stringify(items);
+        localStorage.setItem(storageKey, stringifiedItems);
+    }
 
-    function saveItems() {}
+    function addItem() {
+        const value = input.value;
+        if (!value) {
+            alert('cannot be empty');
+            return;
+        }
 
-    function addItem() {}
+        items.push(value);
+        renderItems();
+        saveItems();
+        input.value = null;
+    }
 
-    function deleteItem(index) {}
+    function deleteItem(index) {
+        items.splice(index, 1);
+        renderItems();
+        saveItems();
+    }
+
+    document.addEventListener("DOMContentLoaded", loadItems)
